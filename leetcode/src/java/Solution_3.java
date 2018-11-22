5b9434aebea9547c2d04c5401783624a8f3f22a9
Emma <17865197939@163.com>
2018-11-22 16:41:47 +0800

    leetCode

--- leetcode/src/java/Solution_3.java
+++ leetcode/src/java/Solution_3.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * @Author:Emma
@@ -10,18 +12,76 @@ import java.util.Map;
 
 public class Solution_3 {
 
-    public void method1(String str){
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String str = in.nextLine();
+
+        System.out.println(method2(str));
+
+    }
+
+    /**
+     * @Author:  Emma
+     * @Description: 暴力遍历，复杂度为O（n^2)
+     * @Date:  2018/11/22 11:20
+     * @Param:
+     * @return:
+     */
+    public static String method1(String str){
 
         int maxLength = 0;
-        Map<Character,Integer> map = new HashMap<>();
+        String res ="";
 
         for (int i = 0; i < str.length(); i++) {
-            Character ele = str.charAt(i);
-            if (map.containsKey(ele)){
-                break;
+            int index = 0;
+            Map<Character,Integer> map = new HashMap<>();
+            for (int j = i; j <str.length() ; j++) {
+                Character ele = str.charAt(j);
+                if (map.containsKey(ele)){
+                    break;
+                }
+                map.put(ele,1);
+                index++;
             }
-            map.put(ele,1);
+
+            if (maxLength<index){
+                maxLength = index;
+                res = str.substring(i,i+maxLength);
+            }
         }
 
+        return res;
+    }
+
+    /**
+     * @Author:  Emma
+     * @Description: 只计算出最长无重复的长度是多少没有截取字符串
+     * @Date:  2018/11/22 14:55
+     * @Param:
+     * @return:
+     */
+
+    public static String method2(String str){
+        int res = 0,left = 0,right = 0;
+
+        HashSet<Character> characters = new HashSet<>();
+
+        while (right<str.length()){
+
+            if (!characters.contains(str.charAt(right))){
+
+                characters.add(str.charAt(right++));
+
+                res = Math.max(res,characters.size());
+
+            }else {
+
+                characters.remove(str.charAt(left++));
+            }
+
+        }
+        return str.substring(left,left+res);
     }
 }
---
+++ leetcode/src/java/Solution_4.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: Emma
+ * @Description:  Median of Two Sorted Arrays 两个有序数组的中位数
+ * @Date: 2018/11/22 15:46
+ **/
+
+public class Solution_4 {
+
+
+}
